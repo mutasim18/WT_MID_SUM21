baca027f78a2823038a34265f89e40cf5795e384
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-06-14 09:59:43 +0600

    5

---
+++ Lab3/problem5.php
@@ -0,0 +1,7 @@
+<?php
+$backColor="green";$fontColor="red"; $imageWidth="80px"; $imageHight="80px";
+?>
+<img src="download.jpg" width="300px" height="300px" />
+<h2>American International University-<span style="background-color:yellow:color:cyan">
+Bangladesh</h2>
+</span>
