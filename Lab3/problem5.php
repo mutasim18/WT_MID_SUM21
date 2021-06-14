7179715a41978b6f857c0682d6a75a771f553df2
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-06-14 11:20:50 +0600

    5

--- Lab3/problem5.php
+++ Lab3/problem5.php
@@ -1,9 +1,9 @@
 <?php
-$backColor="green";$fontColor="red"; $imageWidth="80px"; $imageHight="80px";
+$backColor="green";$fontColor="red"; $imageWidth="100px"; $imageHight="100px";
 ?>
 
 <img src="download.jpg" width="<?php echo $imageWidth; ?>" height="<?php echo $imageHight; ?>" />
 
 <span>
-American International University-<span style="background-color:<?php echo $backColor; ?>;color:<?php echo $fontColor; ?>;">
+American International University-<span style="background-color:<?php echo $backColor; ?>;color:<?php echo $fontColor; ?>;">Bangladesh</span>
 </span>
