50aced754b9af4908bfd75db7836a81066040dd0
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-06-14 09:48:53 +0600

    3

---
+++ Lab3/problem3.php
@@ -0,0 +1,13 @@
+<?php
+$length = 10;
+$width = 10;
+$answer = $length*$width;
+
+echo "The area of the rectangle is = $length*$width = $answer <br>" ;
+
+if($length == $width)
+    {
+        echo "The shape is squre";
+    }
+
+?>
---
+++ Lab3/problem4.php
@@ -0,0 +1,12 @@
+<?php
+$name = "null";
+
+if($name == "raju" && $name == "mina" && $name = "mithu")
+    {
+        echo "Hello";
+    }
+    else
+    {
+        echo "You are not recognized";
+    }
+?>
