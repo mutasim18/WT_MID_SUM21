e6f04f46072b28dbc85ecab8b92a105780900c50
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-06-14 09:20:48 +0600

    1

--- Lab3/problem1.php
+++ Lab3/problem1.php
@@ -3,7 +3,6 @@
         <title>Problem 1</title>
     </head>
     <body>
-        <p>A quick brown <?php echo "<b>fox<b>"?> jumps over the lazy <?php echo "<b>dog<b>"?>
-        </p>
+        <p>A quick brown <?php echo "<b>fox<b>"?> jumps over the lazy <?php echo "<b>dog<b>"?></p>
     </body>
 </html>
---
+++ Lab3/problem2.php
@@ -0,0 +1,26 @@
+<?php
+$marks = 50;
+
+if($marks >= 90)
+    {
+        echo "Grade is A+";
+    }
+    if($marks >= 80 && $marks <90)
+    {
+        echo "Grade is A";
+    }    
+    if($marks >= 70 && $marks <80)
+    {
+        echo "Grade is B";
+    }   
+    if($marks >= 60 && $marks <70)
+    {
+        echo "Grade is C";
+    }    
+    if($marks <60)
+    {
+        echo "Grade is F";
+    }    
+    
+    
+?>
