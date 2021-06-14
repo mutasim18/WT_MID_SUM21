132a5af3e1a67c8c5c7a30b1879e2f3de6954bf8
mutasim18 <57852193+mutasim18@users.noreply.github.com>
2021-06-14 08:56:59 +0600

    1

---
+++ Lab3/problem1.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Problem 1</title>
+    </head>
+    <body>
+        <p>A quick brown <?php echo "<b>fox<b>"?> jumps over the lazy <?php echo "<b>dog<b>"?>
+        </p>
+    </body>
+</html>
